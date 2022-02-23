4fe8ee58ee4889685154a3e305ec73d7a28f3af9
lingsat <lingsatpilingast@gmail.com>
2022-02-23 09:00:26 +0200

    Adding burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let burgerBtn = document.querySelector('.menu__btn');
+let burgerNav = document.querySelector('.navigation');
+
+// Showing navigation menu on burger button click
+burgerBtn.addEventListener('click', function() {
+    burgerBtn.classList.toggle('bg__red');
+    burgerNav.classList.toggle('burger__show');
+});
